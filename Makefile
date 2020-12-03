d977c767ec6bd4c54f1a5b11bff3cac348cc9905
Mike Neff <mjneff2@illinois.edu>
2020-12-03 17:41:31 -0600

    Reading file into graph

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
 main : main.o data.o
-	clang++ main.o data.o -o main
+	clang++ main.o data.o graph.o -o main
 
 main.o : main.cpp
 	clang++ -c main.cpp
 
 data.o: data.cpp
 	clang++ -c data.cpp
+
+graph.o: graph.cpp
+	clang++ -c graph.cpp
 
 clean : 
 	rm *.o main
--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iterator>
+#include <unordered_set>
+#include <queue>
 #include "data.h"
+#include "graph.h"
 
 using namespace std;
 
 int main() {
-    DataSet titles = DataSet("small_sample.tsv");
+    /*DataSet titles = DataSet("small_sample.tsv");
     titles.print();
-    return 0;
+    return 0;*/
+    Graph g(false, true);
+    Vertex start = "";
+    std::ifstream infile("soc-redditHyperlinks-title.tsv");
+    std::string line;
+    std::getline(infile, line); // Get rid of first line
+    while (std::getline(infile, line)) {
+        std::istringstream buffer(line);
+        std::vector<std::string> results((std::istream_iterator<std::string>(buffer)), // Parse string into vector separated by spaces
+                                 std::istream_iterator<std::string>());
+        if (start == "") {
+            start = results[0];
+        }
+        if (!g.vertexExists(results[0])) {
+            g.insertVertex(results[0]); // Insert source vertex if it doesn't exist
+        }
+        if (!g.vertexExists(results[1])) {
+            g.insertVertex(results[1]); // Insert destination vertex if it doesn't exists
+        }
+        g.insertEdge(results[0], results[1]); // Insert edge from that line
+    }
+
+    std::unordered_set<std::string> traversed;
+    std::queue<std::string> queue;
+    queue.push(start);
+    while (!queue.empty()) {
+        std::string current = queue.front();
+        std::cout << current << std::endl;
+        queue.pop();
+
+        for (std::string )
+    }
+
+
 }
