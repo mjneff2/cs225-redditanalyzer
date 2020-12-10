44e880526dbc7a6056b351ec785a6e58f50d88a8
Mike Neff <mjneff2@illinois.edu>
2020-12-10 17:46:25 -0600

    Updated main to read file from command line arg and take user input for algos

--- main.cpp
+++ main.cpp
@@ -9,8 +9,53 @@
 
 using namespace std;
 
-int main() {
-    Graph g(false, true);
-    g.loadData("soc-redditHyperlinks-title.tsv");
-    g.BFS();
+int main(int argc, char *argv[]) {
+    Graph g(true, true);
+    if (argc == 2) {
+        ifstream f(argv[1]);
+        if (f.good()) {
+            string header;
+            getline(f, header);
+            if (header.find("SOURCE_SUBREDDIT") == string::npos || header.find("TARGET_SUBREDDIT") == string::npos) {
+                cout << "File is not a valid dataset." << endl;
+                return -1;
+            }
+            cout << "Parsing dataset..." << endl;
+            g.loadData(argv[1]);
+        } else {
+            cout << "File does not exist." << endl;
+            return -1;
+        }
+    } else {
+        cout << "Arguments should only be one filename." << endl;
+        return -1;
+    }
+    while (true) {
+        cout << "Select algorithm (\"bfs\", \"djikstra\", \"landmark\") or \"end\" to end: ";
+        string choice;
+        cin >> choice;
+        if (choice == "bfs") {
+            g.BFS();
+        } else if (choice == "djikstra") {
+            string start, end;
+            cout << "Enter start: ";
+            cin >> start;
+            cout << "Enter end: ";
+            cin >> end;
+            g.Djikstra(start, end);
+        } else if (choice == "landmark") {
+            string start, end, landmark;
+            cout << "Enter start: ";
+            cin >> start;
+            cout << "Enter end: ";
+            cin >> end;
+            cout << "Enter landmark: ";
+            cin >> landmark;
+            g.Landmark(start, landmark, end);
+        } else if (choice == "end") {
+            return 0;
+        } else {
+            cout << "Invalid choice." << endl;
+        }
+    }
 }
