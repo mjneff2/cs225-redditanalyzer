4a2e08673cc5c2073b2ef56ecb0925ca3baa96fa
Luis <luisap2@illinois.edu>
2020-12-11 19:49:54 -0600

    Added a another sample which should take a diffrent path

--- test.cpp
+++ test.cpp
@@ -16,6 +16,16 @@ TEST_CASE("Djikstra algorithm returns the shortest path") {
 
     REQUIRE(g.DjikstraPath("first", "third") == correctPath);
 }
+TEST_CASE("Another shortest path test with a diffrence sample") {
+    Graph g(true, true);
+    g.loadData("sample2.tsv");
+    list<Vertex> correctPath;
+    correctPath.push_back("first");
+    correctPath.push_back("second");
+    correctPath.push_back("third");
+    
+    REQUIRE(g.DjikstraPath("first", "third") == correctPath);
+}
 TEST_CASE("Djikstra algorithm returns the shortest path - Second Check") {
     Graph g(true, true);
     g.loadData("testcase2.tsv");
